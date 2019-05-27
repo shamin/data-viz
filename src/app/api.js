c1da0e684ee51a1968abf16a4c7d7a8be7cd39ae
shamin.meerankutty <shamin.meerankutty@fullcontact.com>
2019-05-27 22:02:21 +0530

    Using axios to fetch data from the api

---
+++ src/app/api.js
@@ -0,0 +1,1 @@
+export const data_endpoint = "http://www.mocky.io/v2/5cd04a20320000442200fc10"
--- src/app/index.js
+++ src/app/index.js
@@ -1,9 +1,25 @@
-import React from "react"
+import React, {useState, useEffect} from "react"
+import axios from "axios"
+import { data_endpoint } from "./api"
 
 function App() {
+  const [data, setData] = useState({records: [], error: false})
+
+  useEffect(() => {
+    async function fetchData() {
+      try {
+        const response = await axios(data_endpoint)
+        setData({records: response.data, error: true})
+      } catch(e) {
+        setData({records: [], error: true})
+      }
+    }
+    fetchData()
+  }, [])
+
   return (
     <div className="App">
-      Hello
+      {data.records.map((d)=><p key={d.timestamp}>{d.game}</p>)}
     </div>
   )
 }
