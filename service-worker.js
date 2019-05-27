7ca9a7f4fc31457c9892681f02034a38ae8b7b7f
Deployment Bot (from Travis CI) <deploy@travis-ci.org>
2019-05-27 20:57:57 +0000

    Deploy shaminmeerankutty/data-viz to github.com/shaminmeerankutty/data-viz.git:gh-pages

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.247f331ffebfe2cf965dd176b183aa78.js"
+  "/data-viz/precache-manifest.3c8e9e5c2d7efe7fd5b410087292d9cc.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/data-viz/index.html"), {
   
   blacklist: [/^\/_/,/\/[^\/]+\.[^\/]+$/],
 });
